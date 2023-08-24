dedf1f9d750e9a35ae787c2675d2aa02ba76c843
ÖMER GÜNDOĞDU <omergundogdu75@gmail.com>
2023-08-24 17:18:44 +0300

    Python ile Veri Analizi: Gelişmiş Fonksiyonel Keşifçi Veri Analizi başlandı

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -151,7 +151,7 @@ import pandas as pd
 
 s = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])  # index bilgisiyle liste verilerini pandas serisi oluştururur
 type(s)
-pd.Series({"Paris":[10], "Berlin":[20]})
+pd.Series({"Paris": [10], "Berlin": [20]})
 s.index  # RangeIndex(start=0, stop=11, step=1)
 s.dtype
 s.size
@@ -404,16 +404,16 @@ df1 = pd.DataFrame({
 
 df2 = pd.DataFrame({
     'employees': ["john", "dennis", "mark", "maria"],
-    'start_date': [2010,2011,2012,2013]
+    'start_date': [2010, 2011, 2012, 2013]
 })
 
-df3 = pd.merge(df1,df2) # neye göre birleştirme işlemini vermedirk eğer verirsek on kullanıırz
-pd.merge(df1,df2,on="employees")
-df4 = pd.DataFrame({ 'group': ["accounting", "engineering", "hr"],
-                     'manager':["Caner","Mustafa","Berkcan"]})
+df3 = pd.merge(df1, df2)  # neye göre birleştirme işlemini vermedirk eğer verirsek on kullanıırz
+pd.merge(df1, df2, on="employees")
+df4 = pd.DataFrame({'group': ["accounting", "engineering", "hr"],
+                    'manager': ["Caner", "Mustafa", "Berkcan"]})
 df3.columns
 # Her çalışanınn  müdür bilgisini istiyoruz.
-pd.merge(df3,df4) # groupa göre getiriri
+pd.merge(df3, df4)  # groupa göre getiriri
 # genellikle bu işlemler veri tabanında yapılır. tekilleştirilmiş işlemler getirilir.
 
 
@@ -423,5 +423,212 @@ pd.merge(df3,df4) # groupa göre getiriri
 
 
 #####################################################################
-# gelişmiş fonksiyonel keşifci  veri analizi(advanced func exploratory data analysis)
+# matplotlib: veri görselleştirmenin atasıdır. low level bir kütüphanedir., seaborn ise high leveldir.
+#####################################################################
+
+# kategorik değişken: sutun grafiği ile değiştiriyoruz. countplot bar
+# sayısal değşiken: hist, boxplot, -> veri görselleştirme için python uygun değildir. BI araçları daha iyidir.
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["sex"].value_counts().plot(kind="bar")
+plt.show()
+
+# sayısal değer görselleştirme
+plt.hist(df['age'])
+plt.show()
+
+plt.boxplot(df["fare"])
+plt.show()
+#####################################################################
+# Matplotlib özellikleri
+#####################################################################
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+# katmanlı şekilde veri görselleştirme yapar
+# plot özelliği: veriyi görselletirmek için kullanılır.
+x = np.array([1, 8]);
+y = np.array([0, 150])
+
+plt.plot(x, y)
+plt.show()
+
+plt.plot(x, y, "o")
+plt.show()
+
+x = np.array([2, 4, 6, 8, 10]);
+y = np.array([1, 3, 5, 7, 8])
+plt.plot(x, y)
+plt.plot(x, y, "o")
+
+plt.show()
+
+
+
+#####################################################################
+# marker
+#####################################################################
+y = np.array([13, 3, 35, 7, 58])
+
+plt.plot(y,marker="*")
+plt.show()
+
+markers = ['o','*','.',',','x',"X","+","P","s","D","d","H","h"]
+
+#####################################################################
+# line: çizgi özelleği oluşturmak için kullanılır,
+#####################################################################
+y = np.array([14,12,44,3])
+plt.plot(y,linestyle="dashed")
+plt.show()
+
+y = np.array([14,12,44,3])
+plt.plot(y,linestyle="dotted")
+plt.show()
+
+y = np.array([14,12,44,3])
+plt.plot(y,linestyle="dashdot",color="r")
+plt.show()
+
+#####################################################################
+# multiplelines:
+#####################################################################
+x = np.array([2, 10, 1, 17, 260])
+y = np.array([13, 3, 35, 7, 58])
+plt.plot(x,linestyle="dashdot",color="g")
+plt.plot(y,linestyle="dashdot",color="r")
+plt.title("Title")
+plt.xlabel("X label")
+plt.ylabel("Y label")
+plt.grid()
+plt.show()
+
+
+#####################################################################
+# SUBPLOT: birden fazla göreselin gösterilmesidir.
+#####################################################################
+x = np.array([2, 10, 1, 17, 260])
+y = np.array([13, 3, 35, 7, 58])
+plt.subplot(1,3,1)
+plt.title("1")
+plt.plot(x,y)
+
+x = np.array([2, 10, 1, 17, 260])
+y = np.array([13, 3, 35, 7, 58])
+plt.subplot(1,3,2)
+plt.title("2")
+plt.plot(x,y)
+
+x = np.array([2, 10, 1, 17, 260])
+y = np.array([13, 3, 35, 7, 58])
+plt.subplot(1,3,3)
+plt.title("2")
+plt.plot(x,y)
+plt.show()
+
+#####################################################################
+# Seaborn ile Veri Görselleştirme. yüksek seviyeli bir veri görselleştirme kütüphanesidir.
+#####################################################################
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+df = sns.load_dataset("tips")
+df.head()
+
+df["sex"].value_counts()
+sns.countplot(x=df["sex"],data=df)
+plt.show()
+
+df["sex"].value_counts().plot(kind="bar")
+plt.show()
+
+#####################################################################
+# Seaborn ile sayısal Veri Görselleştirme.
+#####################################################################
+
+sns.boxplot(x=df["total_bill"])
+plt.show()
+
+df["total_bill"].hist()
+plt.show()
+
+#####################################################################
+#  Python ile Veri Analizi: Gelişmiş Fonksiyonel Keşifçi Veri Analizi
+#####################################################################
+# Genel Resim
+# Kategorik değişken analizi (Analysis of categorical variables)
+# Sayısal değişken analizi (Analysis of numerical variables)
+# Hedef değişken analizi(Analysis of target variables)
+# Korelasyon analizi (Analysis of oorrelation)
+#####################################################################
+#  Genel Resim: Elimize gelen veriyi tanıma, kaç gözlem,kaç değişken, ilk gözlemlere bakalım, değişkenlere bakalım,
+# veri içerisinde eksiklik var mı,varsa hangi değişkende kaçar tane bakalım.
+#####################################################################
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option("display.max_columns",None)
+pd.set_option("display.width",500)
+df = sns.load_dataset("titanic")
+## bir veriseti elimize geldiğinde yapılacaklar
+df.head()
+df.tail()
+df.shape # satır ve sutun bilgisi
+df.info() # tablonun hakkında özet bilgi
+df.columns # değişkenn isimlerini alırız
+df.index # index bilgisini almak istersek
+df.describe().T # sayısal bilgilerin betimsel istatistikleri
+df.isnull().values.any() # tüm veride eksik değer var mı yokmu
+df.isnull().sum() # eksik olan veri hangi sutunda kaçar tane
+
+def check_df(dataframe, head=5):
+    print("########################### Shape ###########################")
+    print(dataframe.shape)
+    print("########################### dtypes ###########################")
+    print(dataframe.dtypes)
+    print("########################### head ###########################")
+    print(dataframe.head(head))
+    print("########################### tail ###########################")
+    print(dataframe.tail(head))
+    print("########################### NA ###########################")
+    print(dataframe.isnull().sum())
+    print("########################### quantiles ###########################")
+    print(dataframe.describe([0,0.05,0.5,0.95,0.99,1]).T)
+
+
+check_df(df)
+
+df = sns.load_dataset("tips")
+check_df(df)
+
+df = sns.load_dataset("glue")
+check_df(df)
+
+
+#####################################################################
+#  Kategorik değişken analizi (Analysis of categorical variables)
+#####################################################################
+
+#####################################################################
+#  Sayısal değişken analizi (Analysis of numerical variables)
+#####################################################################
+
+#####################################################################
+# Hedef değişken analizi(Analysis of target variables)
+#####################################################################
+
+#####################################################################
+# Korelasyon analizi (Analysis of oorrelation)
 #####################################################################
